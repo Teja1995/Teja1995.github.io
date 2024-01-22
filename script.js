cdd260c042dba818d0c28c92bce1460f2945b18d
Teja1995 <pedapudisrteja@gmail.com>
2024-01-22 09:08:55 +0530

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 function generateQuestion() {
-    const operators = ['+', '*', '/'];
+    const operators = ['+', '*', '/', '%'];
     const operator = operators[Math.floor(Math.random() * operators.length)];
     
-    let num1 = Math.floor(Math.random() * 10) + 1;
-    let num2 = Math.floor(Math.random() * 10) + 1;
+    let num1 = getRandomNumber();
+    let num2 = getRandomNumber();
 
     if (operator === '/' && num1 % num2 !== 0) {
         // Ensure division results in a whole number
-        num1 = num2 * Math.floor(Math.random() * 5) + 1;
+        num1 = num2 * getRandomNumber(5) + 1;
     }
 
     const question = `${num1} ${operator} ${num2}`;
-    const answer = eval(question); // Evaluate the expression to get the answer
+    const answer = calculateAnswer(num1, num2, operator);
 
     const questionContainer = document.getElementById('question-container');
     questionContainer.innerHTML = `<p>${question} = ?</p><p>Answer: ${answer}</p>`;
 }
+
+function getRandomNumber(max = 999) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function calculateAnswer(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        case '%':
+            return (num1 * num2) / 100;
+        default:
+            return NaN; // Handle unsupported operators
+    }
+}
+    
